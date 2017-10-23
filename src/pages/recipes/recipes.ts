dffa5d2bf04bf4eac19061224609e5a5dfcd97f8
Carlos Matheu Armengol <info@plastikaweb.com>
2017-10-23 16:49:28 +0200

    recipe view

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { EditRecipePage } from '../edit-recipe/edit-recipe';
+import { Recipe } from '../../models/recipe';
+import { RecipesService } from '../../services/recipes.service';
+import { RecipePage } from '../recipe/recipe';
 
 @IonicPage()
 @Component({
@@ -8,11 +11,20 @@ import { EditRecipePage } from '../edit-recipe/edit-recipe';
   templateUrl: 'recipes.html'
 })
 export class RecipesPage {
+  recipes: Recipe[] = [];
 
-  constructor(private navController: NavController) {}
+  constructor(private navController: NavController, private recipesService: RecipesService) {}
+
+  ionViewWillEnter() {
+    this.recipes = this.recipesService.getRecipes();
+  }
 
   newRecipe() {
     this.navController.push(EditRecipePage, { mode: 'New' });
+  }
+
+  loadRecipe(index: number, recipe: Recipe) {
+    this.navController.push(RecipePage, { index, recipe });
   }
 
 }
